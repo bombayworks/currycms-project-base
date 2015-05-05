ef1ee79bf7e47a53bc32de7c399f1908a4ec2753
Jesper Ek <deadbeef84@gmail.com>
2015-05-05 18:06:16 +0200

    Removed unused values from config file.

--- cms/config/config.sample.php
+++ cms/config/config.sample.php
@@ -2,14 +2,12 @@
 return array(
     'name' => 'Curry CMS',
     'adminEmail' => 'webmaster@example.com',
-    'forceDomain' => false,
     'migrationVersion' => 1,
     'updateTranslationStrings' => true,
     'developmentMode' => true,
     'setup' => true,
     'maintenance' => array(
         'enabled' => false,
-        'page' => null,
         'message' => 'Site is down for maintenance.',
     ),
     'backend' => array(
